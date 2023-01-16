c79bd322d60a667bf6496077faa93c858b971186
Milovan99 <milovanjakovljevic78@gmail.com>
2023-01-16 23:42:37 +0100

    Material Design

--- app/src/main/java/com/milovanjakovljevic/exploringlayouts/MainActivity.kt
+++ app/src/main/java/com/milovanjakovljevic/exploringlayouts/MainActivity.kt
@@ -2,10 +2,18 @@ package com.milovanjakovljevic.exploringlayouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_menu)
+    }
+    fun loadConstraintLayout(v: View){
+        setContentView(R.layout.activity_main)
+    }
 
+    fun loadTableLayout(v:View){
+        //setContentView(R.layout.my_table_layout)
     }
 }
